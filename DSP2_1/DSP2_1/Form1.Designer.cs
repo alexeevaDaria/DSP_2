a0315f4cfbe3f1e5bf750e0bb8c9e5096f4d607c
alexeevaDaria <mari.marmeladkina@mail.ru>
2020-01-27 01:40:58 +0300

    Project

---
+++ DSP2_1/DSP2_1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DSP2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(1067, 138);
+            this.trackBar2.Maximum = 768;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(69, 247);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1135, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Phi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1063, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "K";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1216, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1118, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "2";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1015, 560);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1153, 138);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(69, 247);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1481, 584);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Name = "Form1";
+            this.Text = "Lab2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ DSP2_1/DSP2_1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DSP2_1
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.DataVisualization.Charting.Series DataSer_1;
+        System.Windows.Forms.DataVisualization.Charting.Series DataSer_2;
+        System.Windows.Forms.DataVisualization.Charting.Series DataSer_3;
+
+        int K, phi;
+        double[] phc = new double[15];
+        public Form1()
+        {
+            InitializeComponent();
+            BuildGraph();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            BuildGraph();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            BuildGraph();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuildGraph()
+        {
+            chart1.Series.Clear();
+            DataSer_1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            DataSer_1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            DataSer_1.Color = Color.Red;
+            DataSer_1.Name = "Погрешность СКЗ 1";
+            DataSer_2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            DataSer_2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            DataSer_2.Color = Color.Blue;
+            DataSer_2.Name = "Погрешность СКЗ 2";
+            DataSer_3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            DataSer_3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            DataSer_3.Color = Color.Green;
+            DataSer_3.Name = "Погрешность амплитуды";
+            int N = 1024;
+            K = (int)trackBar2.Value;
+           // K = (int)3 * N / 4;
+           // phi = (int)180/32;
+            phi = (int)trackBar1.Value;
+            label3.Text = Convert.ToString(phi);
+            label4.Text = Convert.ToString(K);
+            for (int M = K; M <= 2 * N; M++)
+            {
+                double a = M * 2 * Math.PI / N;
+
+                double rms_1 = 0, rms_2 = 0;
+                double Re = 0, Im = 0;
+                for (int n = 0; n <= M; n++)
+                {
+                    double t = Math.Sin(2 * Math.PI * n / N + (double)phi / 180 * Math.PI);
+                    rms_1 += Math.Pow(t, 2);
+                    rms_2 += t;
+
+                    double t2 = Math.Sin(2 * Math.PI * n / N + (double)phi / 180 * Math.PI);
+                    Re += t * Math.Cos(2 * Math.PI * n / M);
+                    Im += t * Math.Sin(2 * Math.PI * n / M);
+                }
+
+                //double Xk = t2;
+                DataSer_3.Points.AddXY(M, 1 - Math.Sqrt(Math.Pow(2 * Re / M, 2) + Math.Pow(2 * Im / M, 2)));
+                DataSer_1.Points.AddXY(M, 0.707 - Math.Sqrt(rms_1 / (M + 1)));
+                DataSer_2.Points.AddXY(M, 0.707 - (Math.Sqrt(rms_1 / (M + 1) - Math.Pow(rms_2 / (M + 1), 2))));
+            }
+            chart1.ResetAutoValues();
+            chart1.Series.Add(DataSer_1);
+            chart1.Series.Add(DataSer_2);
+            chart1.Series.Add(DataSer_3);
+
+        }
+
+    }
+}
